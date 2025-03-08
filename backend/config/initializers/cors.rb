f268b62d8285036efb1928685603b846cc5f2106
pndesu <tranvanphucnguyen1@gmail.com>
2025-03-08 11:01:05 +1100

    Fix CORS configuring

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,16 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://where-is-waldo-phi.vercel.app/'
+    origins 'https://where-is-waldo-phi.vercel.app'
 
     resource "*",
       headers: :any,
+      expose: ["Access-Control-Allow-Origin"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
